9a86eb1f1ccbaa96230cb625b475088c1a44ee96
Mykolaj Krusser <32041427+MykolajKrusser@users.noreply.github.com>
2018-09-16 14:44:08 +0200

    use redux instead locale state in component

---
+++ src/containers/Persons.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import * as actionType from '../store/actions';
+
+import Person from '../components/Person/Person';
+import AddPerson from '../components/AddPerson/AddPerson';
+
+class Persons extends Component {
+    render () {
+        return (
+            <div>
+                <AddPerson personAdded={this.props.onAddedPerson} />
+                {this.props.prs.map(person => (
+                    <Person 
+                        key={person.id}
+                        name={person.name} 
+                        age={person.age} 
+                        clicked={() => this.props.onRemovePerson(person.id)}/>
+                ))}
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = state =>{
+    return{
+        prs: state.persons
+    }
+}
+const mapDispatchToProps = dispatch =>{
+    return{
+        onAddedPerson: ()=>dispatch({type: actionType.ADD_PERSON}),
+        onRemovePerson: (id)=>dispatch({type: actionType.REMOVE_PERSON, personId: id})
+    }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Persons);
