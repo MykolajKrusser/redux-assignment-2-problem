b4ba3e93e7fd1c4304ca98c6dc34484a7c5fb86d
Nicolas <nicolaskrusser@gmail.com>
2018-09-16 15:29:42 +0200

    combining local UI state and redux

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -9,8 +9,8 @@ const reducer = (state = initialState, action)=>{
         case actionTypes.ADD_PERSON:
         const newPerson = {
             id: Math.random(), // not really unique but good enough here!
-            name: 'Nick',
-            age: Math.floor( Math.random() * 30 )
+            name: action.personData.name,
+            age: action.personData.age
         }
             return{
                 ...state,
